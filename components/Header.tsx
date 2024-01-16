469907939b3e180546e2fc3b0f83d3b497d9762f
seanmckee <seanmingmckee@gmail.com>
2024-01-16 02:18:04 -0800

    home page

---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+import { SignInButton, SignedOut } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import React from "react";
+import { ThemeToggler } from "./ThemeToggler";
+import { Boxes } from "lucide-react";
+
+export const Header = () => {
+  return (
+    <header className="flex items-center py-2 fixed justify-between w-screen">
+      <Link className="flex items-center space-x-2 pl-5" href="/">
+        <div className=" w-fit">
+          <Boxes />
+        </div>
+        <h1 className="font-bold text-xl ">SquadSync</h1>
+      </Link>
+
+      <div className="px-5 flex space-x-2 items-center">
+        <div className="px-5 flex space-x-2 items-center">
+          <ThemeToggler />
+          <UserButton afterSignOutUrl="/" />
+          <SignedOut>
+            <SignInButton afterSignInUrl="/" mode="modal" />
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  );
+};
