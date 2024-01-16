c091213cacafe7c00f121f03f005427599303190
seanmckee <seanmingmckee@gmail.com>
2024-01-16 02:38:08 -0800

    homepage changed

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,41 @@ import { Boxes } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-y-10 items-center justify-center h-screen">
+    // <main className="flex flex-col gap-y-10 items-center justify-center h-screen">
+    //   <div className="animate-bounce duration-3000">
+    //     <Boxes size={150} />
+    //   </div>
+
+    //   <div className="flex flex-col lg:flex-row sm:w-[95%] max-w-[75%] gap-5 items-center">
+    //     <div className="flex-1 text-right text-9xl font-bold">
+    //       Squad <br /> Sync
+    //     </div>
+    //     <div className="flex-1 text-3xl flex flex-col items-start gap-20">
+    //       <p>Stay Connected, Sync Up, Squad Up!</p>
+    //       <p>Where Moods Meet Moments and Friendship Finds its Rhythm</p>
+    //     </div>
+    //   </div>
+    //   <Button className="text-white text-lg py-7 rounded-2xl" size="lg">
+    //     Get Started!
+    //   </Button>
+    // </main>
+    <main className="flex flex-col gap-y-10 items-center justify-center h-screen text-center">
       <div className="animate-bounce duration-3000">
         <Boxes size={150} />
       </div>
-      {/* brand */}
-      <div className="flex flex-col lg:flex-row sm:w-[95%] max-w-[75%] gap-5 items-center">
-        <div className="flex-1 text-right text-9xl font-bold">
+
+      <div className="flex flex-col  sm:w-[95%] max-w-[75%] gap-5 items-center">
+        <div className="flex-1  text-9xl font-bold">
           Squad <br /> Sync
         </div>
-        <div className="flex-1 text-3xl flex flex-col  justify-center">
+        <div className="flex-1 text-3xl flex flex-col items-start">
           <p>Stay Connected, Sync Up, Squad Up!</p>
-          <p>Where Moods Meet Moments and Friendship Finds its Rhythm</p>
         </div>
+        <p className="text-lg">
+          Where Moods Meet Moments and Friendship Finds its Rhythm
+        </p>
       </div>
+
       <Button className="text-white text-lg py-7 rounded-2xl" size="lg">
         Get Started!
       </Button>
