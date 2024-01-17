d7ad24e39d7a30324c68643b1150af1bf44cc302
seanmckee <seanmingmckee@gmail.com>
2024-01-17 00:14:56 -0800

    added onboarding form

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,6 +1,9 @@
 import AccountProfile from "@/components/forms/AccountProfile";
+import { OnboardingSchema } from "@/validations/user";
 import { currentUser } from "@clerk/nextjs";
+import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
+import { z } from "zod";
 
 const Onboarding = async () => {
   const user = await currentUser();
@@ -13,7 +16,7 @@ const Onboarding = async () => {
   console.log(userData);
   return (
     <div className="pt-[56px]">
-      <AccountProfile />
+      <AccountProfile userData={userData} />
     </div>
   );
 };
--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,6 +1,85 @@
 "use client";
-const AccountProfile = () => {
-  return <div>AccountProfile</div>;
+
+import { OnboardingSchema } from "@/validations/user";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+interface AccountProfileProps {
+  userData: {
+    id: string | undefined;
+    email: string | undefined;
+  };
+}
+
+const AccountProfile: React.FC<AccountProfileProps> = ({ userData }) => {
+  const form = useForm<z.infer<typeof OnboardingSchema>>({
+    defaultValues: {
+      email: userData.email,
+      name: "",
+    },
+    resolver: zodResolver(OnboardingSchema),
+  });
+
+  function onSubmit(values: z.infer<typeof OnboardingSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter Your Email Here" {...field} />
+                </FormControl>
+                <FormDescription>
+                  Use this email to log in to your account.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter Your Name Here" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is the name your friends will see. Make sure they know
+                  who you are!
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button className="text-white" type="submit">
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default AccountProfile;
