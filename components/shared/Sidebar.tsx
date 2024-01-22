03b297f364efa377c9eb77970512886072edf735
seanmckee <seanmingmckee@gmail.com>
2024-01-22 09:28:33 -0800

    changed light mode sidebar nav hover highlight color

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -19,27 +19,27 @@ const Sidebar = () => {
     <div className="pt-[100px] p-5 shadow-md h-full">
       <ul className="list-none flex flex-col gap-2 font-semibold">
         <Link href="/dashboard">
-          <li className="  cursor-pointer hover:text-white hover:bg-blue-600 dark:hover:bg-[#0D1625] rounded-md p-2 sm:p-5 flex text-xl items-center">
+          <li className="cursor-pointer dark:hover:text-white hover:bg-[#F8FAFC] dark:hover:bg-[#0D1625] rounded-md p-2 sm:p-5 flex text-xl items-center">
             <LayoutDashboard />
             <p className="pl-2 hidden sm:block">Dashboard</p>
           </li>
         </Link>
         <Link href="/groups">
-          <li className="cursor-pointer hover:text-white hover:bg-blue-600 dark:hover:bg-[#0D1625] rounded-md p-5 flex text-xl items-center">
+          <li className="cursor-pointer dark:hover:text-white hover:bg-[#F8FAFC] dark:hover:bg-[#0D1625] rounded-md p-2 sm:p-5 flex text-xl items-center">
             <HeartHandshake />
             <p className="pl-2">Groups</p>
           </li>
         </Link>
 
         <Link href="/friends">
-          <li className="cursor-pointer hover:text-white hover:bg-blue-600 dark:hover:bg-[#0D1625] rounded-md p-5 flex text-xl items-center">
+          <li className="cursor-pointer dark:hover:text-white hover:bg-[#F8FAFC] dark:hover:bg-[#0D1625] rounded-md p-2 sm:p-5 flex text-xl items-center">
             <Users />
             <p className="pl-2">Friends</p>
           </li>
         </Link>
 
         <Link href="/account">
-          <li className="cursor-pointer hover:text-white hover:bg-blue-600 dark:hover:bg-[#0D1625] rounded-md p-5 flex text-xl items-center">
+          <li className="cursor-pointer dark:hover:text-white hover:bg-[#F8FAFC] dark:hover:bg-[#0D1625] rounded-md p-2 sm:p-5 flex text-xl items-center">
             <User />
             <p className="pl-2">Account</p>
           </li>
@@ -47,7 +47,7 @@ const Sidebar = () => {
 
         <li
           onClick={() => signOut(() => router.push("/"))}
-          className="cursor-pointer hover:text-white hover:bg-blue-600 dark:hover:bg-[#0D1625] rounded-md p-5 flex text-xl items-center"
+          className="cursor-pointer dark:hover:text-white hover:bg-[#F8FAFC] dark:hover:bg-[#0D1625] rounded-md p-2 sm:p-5 flex text-xl items-center"
         >
           <LogOut />
           <p className="pl-2">Logout</p>
